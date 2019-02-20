85e384d3f061d44601e36be7e719a87ca7ac8744
Corey Angelus <angelusproductions@gmail.com>
2019-02-20 15:02:21 -0500

    attribute name change in seeds.rb fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'json'
 
 json = File.read('seats.json')
 seatsArray = JSON.parse(json)
-seatsArray.sort_by!{ |seat| [seat["row"], seat["seat"]]}
+seatsArray.sort_by!{ |seat| [seat["row"], seat["letter"]]}
 
 seatsArray.each do |seat|
   Seat.create!(seat)
